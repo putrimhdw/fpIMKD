87263cf035912eb85beb4ef84d5c87b965f46f0f
Putri Mahadewi <mahadewicahayaputri@gmail.com>
2025-12-11 23:56:55 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1 class="mb-3">Selamat Datang di Website Laravel</h1>
+    <h1 class="mb-3">Selamat Datang di Cookora</h1>
     <p>Ini adalah halaman beranda yang memakai layout utama.</p>
 @endsection
